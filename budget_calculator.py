41f29cf29d73d2d322c4b56efdd4ff1a1e7d5804
drah <drah2easy@gmail.com>
2021-02-20 16:48:37 +0800

    First complete solution

--- budget_calculator.py
+++ budget_calculator.py
@@ -1,9 +1,26 @@
+import datetime
+from datetime import timedelta
+from calendar import monthrange
+
 from budget_repo import get_all
 
 
 class BudgetCalculator(object):
     def __init__(self):
-        self.data_source = get_all()
+        data_source = get_all()
+        self.data_source = {item['yearmonth']: item['amount'] for item in data_source}
 
-    def query(self, start, end) -> float:
-        return 31
+    def query(self, start: datetime.date, end: datetime.date) -> float:
+        # date.strftime(%Y%m) -> yyyymm
+        amount = 0
+        delta_days = (end - start).days + 1
+        for d in range(delta_days):
+            cur_day = start + timedelta(days=d)
+            amount += self.get_day_amount_of_month(cur_day)
+        return amount
+
+    def get_days_of_month(self, _date):
+        return monthrange(_date.year, _date.month)[1]
+
+    def get_day_amount_of_month(self, _date):
+        return self.data_source.get(_date.strftime("%Y%m"), 0) / self.get_days_of_month(_date)
--- budget_calculator_test.py
+++ budget_calculator_test.py
@@ -23,6 +23,92 @@ class BudgetCalculatorTest(unittest.TestCase):
         budget = calculator.query(date(2021, 1, 1), date(2021, 1, 31))
         self.assertEqual(31, budget)
 
+    def test_two_months_full(self):
+        self.fake_get_all.return_value = [
+            {"yearmonth": "202101",
+             "amount": 31},
+            {"yearmonth": "202102",
+             "amount": 280},
+        ]
+        calculator = BudgetCalculator()
+        budget = calculator.query(date(2021, 1, 1), date(2021, 2, 28))
+        self.assertEqual(311, budget)
+
+    def test_one_day(self):
+        self.fake_get_all.return_value = [
+            {"yearmonth": "202101",
+             "amount": 31},
+            {"yearmonth": "202102",
+             "amount": 280},
+        ]
+        calculator = BudgetCalculator()
+        budget = calculator.query(date(2021, 1, 1), date(2021, 1, 1))
+        self.assertEqual(1, budget)
+
+    def test_multiple_days(self):
+        self.fake_get_all.return_value = [
+            {"yearmonth": "202101",
+             "amount": 31},
+            {"yearmonth": "202102",
+             "amount": 280},
+        ]
+        calculator = BudgetCalculator()
+        budget = calculator.query(date(2021, 1, 1), date(2021, 1, 2))
+        self.assertEqual(2, budget)
+
+    def test_cross_months(self):
+        self.fake_get_all.return_value = [
+            {"yearmonth": "202101",
+             "amount": 31},
+            {"yearmonth": "202102",
+             "amount": 280},
+        ]
+        calculator = BudgetCalculator()
+        budget = calculator.query(date(2021, 1, 30), date(2021, 2, 1))
+        self.assertEqual(12, budget)
+
+    def test_one_day_given_null(self):
+        self.fake_get_all.return_value = [
+
+        ]
+        calculator = BudgetCalculator()
+        budget = calculator.query(date(2021, 1, 1), date(2021, 1, 1))
+        self.assertEqual(0, budget)
+
+    def test_cross_months_given_one_month_null(self):
+        self.fake_get_all.return_value = [
+            {"yearmonth": "202101",
+             "amount": 31},
+            {"yearmonth": "202103",
+             "amount": 310},
+        ]
+        calculator = BudgetCalculator()
+        budget = calculator.query(date(2021, 1, 31), date(2021, 3, 2))
+        self.assertEqual(21, budget)
+
+    def test_start_bigger_than_end(self):
+        self.fake_get_all.return_value = [
+            {"yearmonth": "202101",
+             "amount": 31},
+            {"yearmonth": "202103",
+             "amount": 310},
+        ]
+        calculator = BudgetCalculator()
+        budget = calculator.query(date(2021, 1, 2), date(2021, 1, 1))
+        self.assertEqual(0, budget)
+
+    def test_cross_year(self):
+        self.fake_get_all.return_value = [
+            {"yearmonth": "202101",
+             "amount": 310},
+            {"yearmonth": "202012",
+             "amount": 31},
+        ]
+        calculator = BudgetCalculator()
+        budget = calculator.query(date(2020, 12, 31), date(2021, 1, 2))
+        self.assertEqual(21, budget)
+
+
 
 
 if __name__ == '__main__':
